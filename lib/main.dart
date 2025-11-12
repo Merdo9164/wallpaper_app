9010fa0ff2e6ff029e3ed0fd195c92a81b5b059d
Merdo9164 <153107982+Merdo9164@users.noreply.github.com>
2025-11-12 14:52:31 +0300

    WLLPPRPPDB-133 backend’den çektiğimiz wallpaper listesini basit bir ListView’de gösteren deneme yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'models/wallpaper.dart';
+import 'services/wallpaper_api.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,31 +8,39 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  final api = WallpaperApi(baseUrl: 'http://localhost:7100');
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Wallpapers')),
+        body: FutureBuilder<List<Wallpaper>>(
+          future: api.getAllWallpapers(),
+          builder: (context ,snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child:CircularProgressIndicator());
+            }else if(snapshot.hasError){
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }else if (snapshot.hasData){
+              final wallpapers = snapshot.data!;
+              return ListView.builder(
+                itemCount: wallpapers.length,
+                itemBuilder: (context, index){
+                  final wallpaper = wallpapers[index];
+                  return ListTile(
+                    title: Text(wallpaper.title),
+                    leading: Image.network(wallpaper.url),
+                  );
+                },
+              );
+            }else {
+              return Center(child: Text('No wallpapers found'));
+            }
+          },
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
